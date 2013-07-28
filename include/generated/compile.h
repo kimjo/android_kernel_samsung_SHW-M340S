fe8ea3641ec4d7478efc793840450d1e389207e8
SungHun <sktjdgns1189@naver.com>
2013-07-28 22:14:09 +0900

    Overclock and Mach Correct Netfilter

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 13 */
+/* This file is auto generated, version 15 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#13 PREEMPT Sun Jul 28 20:46:13 KST 2013"
-#define LINUX_COMPILE_TIME "20:46:13"
+#define UTS_VERSION "#15 PREEMPT Sun Jul 28 21:52:25 KST 2013"
+#define LINUX_COMPILE_TIME "21:52:25"
 #define LINUX_COMPILE_BY "sunghun"
 #define LINUX_COMPILE_HOST "sunghun-System-Product-Name"
 #define LINUX_COMPILE_DOMAIN "(none)"
