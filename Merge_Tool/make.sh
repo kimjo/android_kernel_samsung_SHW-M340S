ea0c53d7d3924e4dc1a1f38551382a4766e43b88
SungHun <sktjdgns1189@naver.com>
2013-07-30 14:27:04 +0900

    엨윽,, 필요하지 않은 파일 정리 및 수정

--- Merge_Tool/make.sh
+++ Merge_Tool/make.sh
@@ -1,4 +1,6 @@
 rm ramdisk-new.cpio
 rm boot.img
+rm zImage
+cp ../arch/arm/boot/zImage ./
 ./mkbootfs ./ramdisk > ramdisk-new.cpio
 ./mkbootimg --base "00200000" --kernel zImage --ramdisk ramdisk-new.cpio -o boot.img
